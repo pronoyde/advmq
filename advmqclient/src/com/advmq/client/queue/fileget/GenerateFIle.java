e8087cdfc7b497dc0fd96dee0f31de33fcbc307e
pronoyde <47469235+pronoyde@users.noreply.github.com>
2020-02-02 04:36:47 -0500

    Class to generate the output file from part files
    Class to generate the output file from part files on client side once the get File is requested

---
+++ advmqclient/src/com/advmq/client/queue/fileget/GenerateFIle.java
@@ -0,0 +1,38 @@
+package com.advmq.client.queue.fileget;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class GenerateFIle {
+
+	public void generateFile(String outputfile,String partFiles,int startPartFile, int endPartFile) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("method called");
+		System.out.println(outputfile +" "+partFiles +" " +startPartFile +" "+endPartFile);
+		int count = startPartFile;
+		
+		FileInputStream fileInputStream=null;
+		File generatedFile = new File(outputfile);
+		FileOutputStream fileOutputStream = new FileOutputStream(generatedFile);
+		byte[] byteArray=null;
+		File file=null;
+		
+		while (count <= endPartFile) {
+			
+		file = new File(partFiles+count);
+		
+		byteArray = new byte[(int) file.length()];
+		
+		fileInputStream = new FileInputStream(file);
+		fileInputStream.read(byteArray);
+		fileOutputStream.write(byteArray);
+		count++;
+		
+		}
+		fileInputStream.close();
+		fileOutputStream.close();
+	}
+
+}
